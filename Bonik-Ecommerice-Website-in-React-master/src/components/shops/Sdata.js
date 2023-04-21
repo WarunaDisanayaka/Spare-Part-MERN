d801a08bef623b6f7f71c40269a014d602eb1995
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-04-21 21:30:37 +0530

    initial commit

---
+++ Bonik-Ecommerice-Website-in-React-master/src/components/shops/Sdata.js
@@ -0,0 +1,40 @@
+const Sdata = {
+  shopItems: [
+    {
+      id: 7,
+      cover: "./images/shops/shops-1.png",
+      name: "Head lights",
+      price: "180",
+      discount: "25",
+    },
+    {
+      id: 8,
+      cover: "./images/shops/shops-2.png",
+      name: "Oil filter",
+      price: "120",
+      discount: "10",
+    },
+    {
+      id: 9,
+      cover: "./images/shops/shops-3.png",
+      name: "Lug bolt",
+      price: "20",
+      discount: "50 ",
+    },
+    {
+      id: 10,
+      cover: "./images/shops/shops-4.png",
+      name: "Motor assy",
+      price: "999",
+      discount: "10 ",
+    },
+    {
+      id: 11,
+      cover: "./images/shops/shops-5.png",
+      name: "Fog lamp",
+      price: "80",
+      discount: "20 ",
+    },
+  ],
+};
+export default Sdata;
