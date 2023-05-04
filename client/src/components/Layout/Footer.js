a5ad25137f4a08dc942a7d848f701a332f2cb732
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-05 01:49:48 +0530

    added pages

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="bg-dark text-light p-5">
+      <h6 className='text-center'>
+        All Right Reserved
+      </h6>
+    </div>
+  )
+}
+
+export default Footer
